93c038851a965755efbd9b3e97292a106b4d829b
edwardAugust <edward.aug.watts@gmail.com>
2023-04-13 23:35:18 -0500

    Update logic.js

---
+++ Starter_Code/static/js/logic.js
@@ -0,0 +1,20 @@
+// Use this link to get the GeoJSON data.
+const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_day.geojson'
+
+// Creating the map object
+let myMap = L.map("map", {
+    center: [40.7128, -74.0059],
+    zoom: 11
+  });
+  
+  // Adding the tile layer
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(myMap);
+
+  
+  // Getting our GeoJSON data
+  d3.json(url).then(function(data) {
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJson(data).addTo(myMap);
+  });
