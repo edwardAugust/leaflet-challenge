7038fa09ebbf1b08c7f139d0a9ebd001c90d2945
edwardAugust <edward.aug.watts@gmail.com>
2023-04-18 23:03:41 -0500

    Update logic.js

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,13 +1,13 @@
 // Use this link to get the GeoJSON data.
 const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
 
-function setColor(magnitude){
-  return magnitude > 5? #de163a:
-    magnitude > 4? #cf1593:
-    magnitude > 3? #7810c7:
-    #1b0dd6:
-    #0a52d9:
-    #66cdaa;
+function setColor(depth){
+  return depth > 90? '#de163a':
+    depth > 70? '#cf1593':
+    depth > 50? '#7810c7':
+    depth > 30? '#1b0dd6':
+    depth > 10? '#0a52d9':
+      '#66cdaa';
 }
 
 
@@ -34,9 +34,9 @@ d3.json(url).then(function(data) {
       pointToLayer: function(feature, latlng) {
       // point size and color 
       return new L.CircleMarker(latlng, {
-          radius: feature.properties.mag*5,
-          fillColor: setColor(feature.properties.mag), 
-          fillOpacity: 0.85
+          radius: feature.properties.mag*3.3,
+          fillColor: setColor(feature.geometry.coordinates[2]), 
+          fillOpacity: 0.3
     })
   }});
   map(points) 
